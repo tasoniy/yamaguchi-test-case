277ee35db1e63c3c11c5fad9cd4b752d0298f4b1
Volkova Anastasiya <avftiq@gmail.com>
2022-12-17 19:42:55 +0300

    add animation

---
+++ ergonomics/index.js
@@ -0,0 +1,7 @@
+  const buttonUp = document.querySelector(".button__button-up");
+  const buttonDown = document.querySelector(".button__button-down");
+  const image = document.getElementById("image1")
+
+  buttonUp.addEventListener("click", () => {
+   console.log("Click")
+  })
