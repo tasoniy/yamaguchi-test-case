5cb2881f19da22e766d70a315a26b9ca7f4829b2
Volkova Anastasiya <avftiq@gmail.com>
2022-12-18 23:25:42 +0300

    add up and down table animation

--- ergonomics/index.js
+++ ergonomics/index.js
@@ -1,7 +1,17 @@
   const buttonUp = document.querySelector(".button__button-up");
   const buttonDown = document.querySelector(".button__button-down");
-  const image = document.getElementById("image1")
+
+  const image = document.querySelector(".countertop");
 
   buttonUp.addEventListener("click", () => {
-   console.log("Click")
+    let step = parseInt(image.style.bottom) || 0;
+    image.style.bottom = step + 15 + "px";
+    while(image.style.bottom < 90) {
+      image.style.bottom = step + 15 + "px";
+    }
   })
+
+  buttonDown.addEventListener("click", () => {
+    let step= parseInt(image.style.bottom) || 0;
+    image.style.bottom = step - 15 + "px";
+   })
